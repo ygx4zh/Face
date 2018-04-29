a791b99c8664bb34b6769cf0951a7ef7d13aa529
YGX <sinsyet@163.com>
2018-04-29 18:35:25 +0800

    update

--- FaceSample/src/main/java/com/example/facesample/activities/VerifyActivity.java
+++ FaceSample/src/main/java/com/example/facesample/activities/VerifyActivity.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +22,23 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.arcsoft.facerecognition.AFR_FSDKFace;
 import com.example.facesample.R;
+import com.example.facesample.adapters.FolderAdapter;
 import com.example.facesample.adapters.SimilarAdapter;
+import com.example.facesample.compute.FaceBean;
+import com.example.facesample.compute.FaceVerify;
+import com.example.facesample.db.DBManager;
+import com.example.facesample.db.bean.FaceImageBean;
 import com.example.facesample.engine.imgscan.GallyPageTransformer;
 import com.example.facesample.ui.views.DisplayImageView;
 import com.example.facesample.utils.AnimUtil;
+import com.example.facesample.utils.AppHelper;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class VerifyActivity extends AppCompatActivity implements View.OnClickListener {
     public final static int FILE = 1;
@@ -37,6 +49,22 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
     private View mVSimilar;
     private View mLl;
     private View mFl;
+    private Bitmap bitmap;
+
+
+    private static final int UPDATE_FACE_COUNT = 1;
+    private Handler mHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_FACE_COUNT:
+                    mTvCount.setText(String.format(Locale.CHINA,photosFormat,(int)msg.obj));
+                    break;
+            }
+        }
+    };
+    private TextView mTvCount;
+    private String photosFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +78,57 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void initData() {
+
+        photosFormat = getString(R.string.similar_photos_format);
+
         Intent intent = getIntent();
 
         int type = intent.getIntExtra("type", 0);
         switch (type){
             case FILE:
                 String path = intent.getStringExtra("path");
-                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                bitmap = BitmapFactory.decodeFile(path);
                 mIv.setImageBitmap(bitmap);
                 break;
         }
+
+        AppHelper.run(searchSimilar);
     }
+
+    private Runnable searchSimilar = new Runnable() {
+        @Override
+        public void run() {
+            AFR_FSDKFace afr_fsdkFace = FaceVerify.extraBitmapFeature(bitmap);
+            if(afr_fsdkFace == null){
+                Log.e(TAG, "run: 未找到人脸");
+                return;
+            }
+            List<FaceImageBean> faceImageBeans = DBManager.queryFaceImages();
+            Log.e(TAG, "run: "+faceImageBeans.size());
+            for (FaceImageBean bean : faceImageBeans) {
+                String face_token = bean.getFace_token();
+                if(TextUtils.isEmpty(face_token))
+                {
+                    Log.e(TAG, "run: face token is null");
+                    continue;
+                }
+
+                FaceBean face = FaceBean.decodeFile(face_token);
+                if (face == null) {
+                    Log.e(TAG, "run: local file is null: "+face_token);
+                    continue;
+                }
+                double cump = FaceVerify.cump(afr_fsdkFace, face.getMface());
+                Log.e(TAG, "run: cump: "+cump);
+                if(cump > 0.5f){
+                    mHandler.sendMessage(Message.obtain(mHandler,UPDATE_FACE_COUNT,++ count));
+                }
+            }
+        }
+    };
+
+    private int count;
+
 
     private void setFullScreen() {
         /*set it to be no title*/
@@ -79,15 +147,13 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
 
     void findView(){
         mIv = findViewById(R.id.verify_iv);
-        mIv.setImageResource(R.mipmap.ai_2);
         mVp = findViewById(R.id.verify_vp);
         mTv = findViewById(R.id.verify_tv_info);
         mVSimilar = findViewById(R.id.verify_tv_similar);
         mLl = findViewById(R.id.verify_ll);
         mFl = findViewById(R.id.verify_fl);
         mVSimilar.setOnClickListener(this);
-        mTv.setText(Html.fromHtml(getString(R.string.info_format)));
-        initFilesData();
+        mTvCount = findViewById(R.id.verify_tv_count);
         mVp.setOffscreenPageLimit(3);
         int pagerWidth = (int) (getResources().getDisplayMetrics().widthPixels * 3.0f / 5.0f);
         ViewGroup.LayoutParams lp = mVp.getLayoutParams();
@@ -140,7 +206,6 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
             mSwitchMode = SWITCHING;
             final int[] position = new int[2];
             mFl.getLocationOnScreen(position);
-//            Log.e(TAG, "switchViewpagerVisibility: "+translationY+" // "+top);
             AnimUtil.playTranslationYOut(mFl, mLl,
                     new ValueAnimator.AnimatorUpdateListener() {
                 @Override
--- FaceSample/src/main/java/com/example/facesample/compute/FaceBean.java
+++ FaceSample/src/main/java/com/example/facesample/compute/FaceBean.java
@@ -2,10 +2,16 @@ package com.example.facesample.compute;
 
 import com.arcsoft.facerecognition.AFR_FSDKFace;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class FaceBean {
 
-    public FaceBean(AFR_FSDKFace fsdkFace, String name){
+    public FaceBean(AFR_FSDKFace fsdkFace, String name) {
         this.mface = fsdkFace;
         this.name = name;
     }
@@ -29,5 +35,31 @@ public class FaceBean {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static FaceBean decodeFile(String path) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        File file = new File(path);
+        InputStream is = null;
+        FaceBean bean = null;
+        try {
+            is = new FileInputStream(file);
+            int len = -1;
+            byte[] buf = new byte[1024];
+            while ((len = is.read(buf)) != -1) {
+                baos.write(buf);
+                baos.flush();
+            }
+            bean = new FaceBean(new AFR_FSDKFace(baos.toByteArray()),file.getName());
+        } catch (Exception e) {
+        }finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+        return bean;
     }
 }
